b8f091c88dfa6ad5c823d1e60df5a954177ecb40
Mikko Paderes <rmmpaderes@gmail.com>
2019-06-23 21:39:17 +0800

    Add fastboot shim for firebase/app

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 'use strict';
 
+const { map } = require('broccoli-stew');
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: require('./package').name,
+
+  treeForVendor(defaultTree) {
+    let browserVendorLib = new Funnel('node_modules', {
+      destDir: 'fastboot-shims',
+      files: ['firebase/firebase-app.js'],
+    });
+
+    browserVendorLib = map(browserVendorLib, content => (
+      `if (typeof FastBoot === 'undefined') { ${content} }`
+    ));
+
+    return new MergeTrees([defaultTree, browserVendorLib]);
+  },
 
   included(app) {
     this._super.included.apply(this, arguments);
 
-    app.import('node_modules/firebase/firebase-app.js');
+    app.import('vendor/fastboot-shims/firebase/firebase-app.js');
     app.import('vendor/shims/firebase.js');
   },
 };
