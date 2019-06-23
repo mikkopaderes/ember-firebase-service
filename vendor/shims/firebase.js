80bbb8edd2519cf6d457efac348d7b133ac4e09e
Mikko Paderes <rmmpaderes@gmail.com>
2019-06-23 23:37:51 +0800

    Add fastboot shim for firebase/app (#14)

--- vendor/shims/firebase.js
+++ vendor/shims/firebase.js
@@ -2,17 +2,11 @@
   function vendorModule() {
     'use strict';
 
-    if (typeof FastBoot === 'undefined') {
-      return {
-        'default': self['firebase'],
-        __esModule: true,
-      };
-    } else {
-      return {
-        'default': FastBoot.require('firebase/app'),
-        __esModule: true,
-      };
-    }
+    const firebase = typeof FastBoot === 'undefined'
+      ? self['firebase']
+      : FastBoot.require('firebase/app');
+
+    return { default: firebase, __esModule: true };
   }
 
   define('firebase', [], vendorModule);
